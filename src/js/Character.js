cfc6e1f3366e8878ad43e3a6c03673808ec12a85
Speeddy <speeddy1984@yandex.ru>
2024-08-11 10:22:53 +0300

    js-advanced-diploma 2.0

---
+++ src/js/Character.js
@@ -0,0 +1,40 @@
+/**
+ * Базовый класс, от которого наследуются классы персонажей
+ * @property level - уровень персонажа, от 1 до 4
+ * @property attack - показатель атаки
+ * @property defence - показатель защиты
+ * @property health - здоровье персонажа
+ * @property type - строка с одним из допустимых значений:
+ * swordsman
+ * bowman
+ * magician
+ * daemon
+ * undead
+ * vampire
+ */
+export default class Character {
+  constructor(level, type = 'generic') {
+    this.level = level;
+    this.attack = 0;
+    this.defence = 0;
+    this.health = 50;
+    this.type = type;
+    // TODO: выбросите исключение, если кто-то использует "new Character()"
+    if (new.target === Character) {
+      throw new Error("Нельзя создавать объект класса родителя");
+    }
+  }
+
+  get characterInfo() {
+    return `\u{1F396}${this.level} \u{2694}${this.attack} \u{1F6E1}${this.defence} \u{2764}${this.health}`;
+  }
+
+  levelUp() {
+    if (this.level < 4) {
+      this.level += 1;
+    }
+    this.attack = Math.max(this.attack, this.attack * (80 + this.health) / 100);
+    this.defence = Math.max(this.defence, this.defence * (80 + this.health) / 100);
+    this.health = Math.min(100, this.health + 80);
+  }
+}
